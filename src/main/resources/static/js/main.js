58c0af217f6dae01cda80bd246de2ae9789faf70
sahrulprograming <sahrulsaefulah@gmail.com>
2023-02-01 12:57:18 +0700

    integrated ui billingCost with API

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,20 @@
 const urlApi = "http://localhost:4040/api";
+function formatRupiah(angka, prefix) {
+  var number_string = angka.toString();
+  split = number_string.split(',');
+  sisa = split[0].length % 3;
+  rupiah = split[0].substr(0, sisa);
+  ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+  // tambahkan titik jika yang di input sudah menjadi angka ribuan
+  if (ribuan) {
+    separator = sisa ? '.' : '';
+    rupiah += separator + ribuan.join('.');
+  }
+
+  rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+  return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+}
 
 $(document).ready(function () {
   // Getting data from api with pagination
@@ -196,7 +212,7 @@ $(document).ready(function () {
     formData.forEach((value, key) => {
       data[key] = value;
     });
-    console.log(JSON.stringify(data));
+    // console.log(JSON.stringify(data));
     $.ajax({
       type: "POST",
       url: `${urlApi}/${endPoint}`,
@@ -414,22 +430,6 @@ $(document).ready(function () {
   })
 
   /* Fungsi formatRupiah */
-  function formatRupiah(angka, prefix) {
-    var number_string = angka.toString();
-    split = number_string.split(',');
-    sisa = split[0].length % 3;
-    rupiah = split[0].substr(0, sisa);
-    ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
-    // tambahkan titik jika yang di input sudah menjadi angka ribuan
-    if (ribuan) {
-      separator = sisa ? '.' : '';
-      rupiah += separator + ribuan.join('.');
-    }
-
-    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-    return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-  }
 
   // load data for detail monthly dues
   if (window.location.pathname.includes('/monthlyDues/detail') || window.location.pathname.includes('admin/houses/update')) {
